07ae0d532dd481045b706f4136003fef5634dcf9
Sam Lee-Lindsay <sleelin@gmail.com>
2021-09-09 17:00:33 +1000

    Add: SCIM Schemas router in provisioning integration

--- routers.js
+++ routers.js
@@ -1,5 +1,6 @@
 import {Router} from "express";
 import {Users} from "./routers/users.js";
+import {Schemas} from "./routers/schemas.js";
 
 export class SCIMRouters extends Router {
     constructor(authenticated = ((req, res, next) => next())) {
@@ -7,5 +8,6 @@ export class SCIMRouters extends Router {
         
         this.use(async (req, res, next) => await authenticated(req, res, next));
         this.use(new Users());
+        this.use(new Schemas());
     }
 }
