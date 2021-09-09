c520bfc751037f27f9ce5f3a2de0735921d3ba68
Sam Lee-Lindsay <sleelin@gmail.com>
2021-09-09 17:02:20 +1000

    Add: dynamically set the basepath of the SCIM user and schema resources when the SCIM router is first reached

--- routers.js
+++ routers.js
@@ -1,10 +1,17 @@
 import {Router} from "express";
 import {Users} from "./routers/users.js";
 import {Schemas} from "./routers/schemas.js";
+import * as SCIM from "../scim/scim.js";
 
 export class SCIMRouters extends Router {
     constructor(authenticated = ((req, res, next) => next())) {
         super();
+        
+        this.use("/", (req, res, next) => {
+            SCIM.Resources.User.basepath(req.baseUrl);
+            SCIM.Resources.Schema.basepath(req.baseUrl);
+            next();
+        });
         
         this.use(async (req, res, next) => await authenticated(req, res, next));
         this.use(new Users());
