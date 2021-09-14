0bb78b948b37d0c8173ae5695bcd72940a292756
Sam Lee-Lindsay <sleelin@gmail.com>
2021-09-14 16:59:27 +1000

    Add: safely mount SCIM users router endpoints after checking user resource type implementation is registered

--- routers/users.js
+++ routers/users.js
@@ -5,64 +5,66 @@ export class Users extends Router {
     constructor() {
         super();
         
-        this.get("/Users", async (req, res) => {
-            try {
-                res.send(await new SCIM.Resources.User(req.query).read());
-            } catch (ex) {
-                if (ex instanceof SCIM.Types.Error) {
-                    res.status(ex.status).send(new SCIM.Messages.Error(ex.status, ex.scimType, ex.message));
-                } else {
-                    res.status(500).send(new SCIM.Messages.Error(500, null, ex.message));
+        if (SCIM.Resources.registered(SCIM.Resources.User)) {
+            this.get("/Users", async (req, res) => {
+                try {
+                    res.send(await new SCIM.Resources.User(req.query).read());
+                } catch (ex) {
+                    if (ex instanceof SCIM.Types.Error) {
+                        res.status(ex.status).send(new SCIM.Messages.Error(ex.status, ex.scimType, ex.message));
+                    } else {
+                        res.status(500).send(new SCIM.Messages.Error(500, null, ex.message));
+                    }
                 }
-            }
-        });
-        
-        this.get("/Users/:id", async (req, res) => {
-            try {
-                res.send(await new SCIM.Resources.User(req.params.id, req.query).read());
-            } catch (ex) {
-                if (ex instanceof SCIM.Types.Error) {
-                    res.status(ex.status).send(new SCIM.Messages.Error(ex.status, ex.scimType, ex.message));
-                } else {
-                    res.status(500).send(new SCIM.Messages.Error(500, null, ex.message));
+            });
+            
+            this.get("/Users/:id", async (req, res) => {
+                try {
+                    res.send(await new SCIM.Resources.User(req.params.id, req.query).read());
+                } catch (ex) {
+                    if (ex instanceof SCIM.Types.Error) {
+                        res.status(ex.status).send(new SCIM.Messages.Error(ex.status, ex.scimType, ex.message));
+                    } else {
+                        res.status(500).send(new SCIM.Messages.Error(500, null, ex.message));
+                    }
                 }
-            }
-        });
-        
-        this.post("/Users", async (req, res) => {
-            try {
-                res.status(201).send(await new SCIM.Resources.User(req.query).write(req.body));
-            } catch (ex) {
-                if (ex instanceof SCIM.Types.Error) {
-                    res.status(ex.status).send(new SCIM.Messages.Error(ex.status, ex.scimType, ex.message));
-                } else {
-                    res.status(500).send(new SCIM.Messages.Error(500, null, ex.message));
+            });
+            
+            this.post("/Users", async (req, res) => {
+                try {
+                    res.status(201).send(await new SCIM.Resources.User(req.query).write(req.body));
+                } catch (ex) {
+                    if (ex instanceof SCIM.Types.Error) {
+                        res.status(ex.status).send(new SCIM.Messages.Error(ex.status, ex.scimType, ex.message));
+                    } else {
+                        res.status(500).send(new SCIM.Messages.Error(500, null, ex.message));
+                    }
                 }
-            }
-        });
-    
-        this.put("/Users/:id", async (req, res) => {
-            try {
-                res.send(await new SCIM.Resources.User(req.params.id, req.query).write(req.body));
-            } catch (ex) {
-                if (ex instanceof SCIM.Types.Error) {
-                    res.status(ex.status).send(new SCIM.Messages.Error(ex.status, ex.scimType, ex.message));
-                } else {
-                    res.status(500).send(new SCIM.Messages.Error(500, null, ex.message));
+            });
+            
+            this.put("/Users/:id", async (req, res) => {
+                try {
+                    res.send(await new SCIM.Resources.User(req.params.id, req.query).write(req.body));
+                } catch (ex) {
+                    if (ex instanceof SCIM.Types.Error) {
+                        res.status(ex.status).send(new SCIM.Messages.Error(ex.status, ex.scimType, ex.message));
+                    } else {
+                        res.status(500).send(new SCIM.Messages.Error(500, null, ex.message));
+                    }
                 }
-            }
-        });
-    
-        this.delete("/Users/:id", async (req, res) => {
-            try {
-                res.status(204).send(await new SCIM.Resources.User(req.params.id).dispose());
-            } catch (ex) {
-                if (ex instanceof SCIM.Types.Error) {
-                    res.status(ex.status).send(new SCIM.Messages.Error(ex.status, ex.scimType, ex.message));
-                } else {
-                    res.status(500).send(new SCIM.Messages.Error(500, null, ex.message));
+            });
+            
+            this.delete("/Users/:id", async (req, res) => {
+                try {
+                    res.status(204).send(await new SCIM.Resources.User(req.params.id).dispose());
+                } catch (ex) {
+                    if (ex instanceof SCIM.Types.Error) {
+                        res.status(ex.status).send(new SCIM.Messages.Error(ex.status, ex.scimType, ex.message));
+                    } else {
+                        res.status(500).send(new SCIM.Messages.Error(500, null, ex.message));
+                    }
                 }
-            }
-        });
+            });
+        }
     }
 }
