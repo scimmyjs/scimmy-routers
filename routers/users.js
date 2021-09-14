06cf05b700ae9a2296c9ca48a4ecef45872f442f
Sam Lee-Lindsay <sleelin@gmail.com>
2021-09-14 15:54:00 +1000

    Add: DELETE listener in SCIM users router

--- routers/users.js
+++ routers/users.js
@@ -52,5 +52,17 @@ export class Users extends Router {
                 }
             }
         });
+    
+        this.delete("/Users/:id", async (req, res) => {
+            try {
+                res.status(204).send(await new SCIM.Resources.User(req.params.id).dispose());
+            } catch (ex) {
+                if (ex instanceof SCIM.Types.Error) {
+                    res.status(ex.status).send(new SCIM.Messages.Error(ex.status, ex.scimType, ex.message));
+                } else {
+                    res.status(500).send(new SCIM.Messages.Error(500, null, ex.message));
+                }
+            }
+        });
     }
 }
