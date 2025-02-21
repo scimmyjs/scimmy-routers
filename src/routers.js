0a2601019ede6582b629d40a5f656ac49bdba2f9
Jan Hecker <jan.hecker@mennekes.de>
2025-02-21 10:11:29 +0100

    Add bodyParser as a direct dependency

--- src/routers.js
+++ src/routers.js
@@ -1,4 +1,4 @@
-import express, {Router} from "express";
+import {Router} from "express";
 import SCIMMY from "scimmy";
 import {Resources} from "./routers/resources.js";
 import {Schemas} from "./routers/schemas.js";
@@ -7,6 +7,7 @@ import {ServiceProviderConfig} from "./routers/spconfig.js";
 import {Search} from "./routers/search.js";
 import {Bulk} from "./routers/bulk.js";
 import {Me} from "./routers/me.js";
+import {json} from 'body-parser';
 
 // Re-export SCIMMY for consumption by dependent packages
 export {SCIMMY};
@@ -78,9 +79,9 @@ export class SCIMMYRouters extends Router {
      */
     constructor(authScheme = {}) {
         const {type, docUri, handler, context = (() => {}), baseUri = (() => {})} = authScheme;
-        
+
         super({mergeParams: true});
-        
+
         // Make sure supplied authentication scheme is valid
         if (type === undefined)
             throw new TypeError("Missing required parameter 'type' from authentication scheme in SCIMMYRouters constructor");
@@ -94,37 +95,37 @@ export class SCIMMYRouters extends Router {
             throw new TypeError("Parameter 'context' must be of type 'function' for authentication scheme in SCIMMYRouters constructor");
         if (typeof baseUri !== "function")
             throw new TypeError("Parameter 'baseUri' must be of type 'function' for authentication scheme in SCIMMYRouters constructor");
-        
+
         // Register the authentication scheme, and other SCIM Service Provider Config options
         SCIMMY.Config.set({
             patch: true, filter: true, sort: true, bulk: true,
             authenticationSchemes: [{...authSchemeTypes[type], documentationUri: docUri}]
         });
-        
+
         // Make sure SCIM JSON is decoded in request body
-        this.use(express.json({type: ["application/scim+json", "application/json"], limit: SCIMMY.Config.get()?.bulk?.maxPayloadSize ?? "1mb"}));
-        
+        this.use(json({type: ["application/scim+json", "application/json"], limit: SCIMMY.Config.get()?.bulk?.maxPayloadSize ?? "1mb"}));
+
         // Listen for incoming requests to determine basepath for all resource types
         this.use("/", async (req, res, next) => {
             // Set correct header for SCIM responses
             res.setHeader("Content-Type", "application/scim+json");
-            
+
             try {
                 // Evaluate the request-based basepath location
                 const basepath = await baseUri(req) ?? "";
-                
+
                 // Make sure it's a valid URL string
                 if (!basepath || typeof basepath === "string" && basepath.match(/^https?:\/\//)) {
                     // Construct the actual basepath to use for resource locations...
                     const location = basepath.replace(/\/$/, "") + req.baseUrl;
-                    
+
                     // ...then set all resource basepaths correctly
                     SCIMMY.Resources.Schema.basepath(location);
                     SCIMMY.Resources.ResourceType.basepath(location);
                     SCIMMY.Resources.ServiceProviderConfig.basepath(location);
                     for (let Resource of Object.values(SCIMMY.Resources.declared()))
                         Resource.basepath(location);
-                    
+
                     next();
                 } else {
                     next(new TypeError("Method 'baseUri' must return a URL string in SCIMMYRouters constructor"));
@@ -133,7 +134,7 @@ export class SCIMMYRouters extends Router {
                 next(ex);
             }
         });
-        
+
         // Make sure requests are authenticated using supplied auth handler method
         this.use(async (req, res, next) => {
             try {
@@ -145,7 +146,7 @@ export class SCIMMYRouters extends Router {
                 res.status(401).send(new SCIMMY.Messages.Error({status: 401, message: ex.message}));
             }
         });
-        
+
         // Cast pagination query parameters from strings to numbers...
         this.use(({query}, res, next) => {
             for (let param of ["startIndex", "count"]) {
@@ -154,10 +155,10 @@ export class SCIMMYRouters extends Router {
                     query[param] = +query[param];
                 }
             }
-            
+
             next();
         });
-        
+
         // Register core service provider endpoints
         this.use(new Schemas());
         this.use(new ResourceTypes());
@@ -165,15 +166,15 @@ export class SCIMMYRouters extends Router {
         this.use(new Search(context));
         this.use(new Bulk(context));
         this.use(new Me(handler, context));
-        
+
         // Register endpoints for any declared resource types
         for (let Resource of Object.values(SCIMMY.Resources.declared())) {
             this.use(Resource.endpoint, new Resources(Resource, context));
         }
-        
+
         // If we get to this point, there's no matching endpoints
         this.use((req, res) => res.status(404).send(new SCIMMY.Messages.Error({status: 404, message: "Endpoint Not Found"})));
-        
+
         // Handle any middleware exceptions, and if necessary, forward to next middleware
         this.use((ex, req, res, next) => {
             res.status(ex.status ?? 500).send(new SCIMMY.Messages.Error(ex));
